7d0abfffa12e70bff182fa5f509ed384f84c1c35
Rabiul Alam <rabiul.alam@outlook.com>
2020-12-03 17:07:58 -0500

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Player from './components/Player'
+import Song from './components/Song'
+
+import "./styles/app.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
